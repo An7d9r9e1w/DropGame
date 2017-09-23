06d1f3e2118cb2eaa95a07016c68e304afb1169e
An7d9r9e1w <An7d9r9e1w@mail.ru>
2017-09-23 10:41:00 +0300

    Drop 2.0

--- desktop/src/com/badlogic/drop/desktop/DesktopLauncher.java
+++ desktop/src/com/badlogic/drop/desktop/DesktopLauncher.java
@@ -1,13 +1,14 @@
 package com.badlogic.drop.desktop;
 
+import com.badlogic.drop.Drop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.drop.Drop;
+import com.badlogic.drop.GameScreen;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Drop";
+		config.title = "GameScreen";
 		config.width = 800;
 		config.height = 480;
 		new LwjglApplication(new Drop(), config);
